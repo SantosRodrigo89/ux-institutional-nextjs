d04cfbc8eb063dcc8b472e90307ae647bb4f988e
Rodrigo Vieira Batista dos Santos <rodrigo.vieira@cdcscm.com.br>
2023-12-06 18:57:14 -0300

    feat: instalando mui

--- src/components/organisms/firstHeader/style.js
+++ src/components/organisms/firstHeader/style.js
@@ -3,19 +3,13 @@ import styled from "styled-components";
 export const MainContainer = styled.div`
   display: flex;
   justify-content: space-around;
-  background-color: #6c757d;
+  background-color: #e5e8e8;
 `;
 
-export const WAContainer = styled.div`
+export const WAContainer = styled.div``;
 
-`;
-
-export const EmailContainer = styled.div`
-  
- 
-`;
+export const EmailContainer = styled.div``;
 
 export const ThirdIcons = styled.div`
   display: flex;
-
 `;
