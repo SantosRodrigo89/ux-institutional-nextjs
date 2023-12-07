e8608aa7c4c6808ebd91c6916197c76ada746f7a
Rodrigo Vieira Batista dos Santos <rodrigo.vieira@cdcscm.com.br>
2023-12-06 22:17:25 -0300

    feat: fazendo carrosel

---
+++ src/components/molecules/muiCarousel/Item.jsx
@@ -0,0 +1,12 @@
+import { Paper, Button } from "@mui/material";
+
+function Item() {
+  return (
+    <Paper>
+      <h2>{props.item.name}</h2>
+      <p>{props.item.description}</p>
+
+      <Button className="CheckButton">Check it out!</Button>
+    </Paper>
+  );
+}
